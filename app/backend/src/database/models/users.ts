36cd479228d7809d01126752687ab025978475da
marqsbruno <marqsbruno@gmail.com>
2022-08-24 17:16:50 -0300

    feat: :sparkles: arrumado seeder de user

---
+++ app/backend/src/database/models/users.ts
@@ -0,0 +1,44 @@
+import { INTEGER, Model, STRING } from 'sequelize';
+import db from '.';
+
+class Users extends Model {
+  id!: number;
+  username!: string;
+  role!: string;
+  email!: string;
+  password!: string;
+}
+
+Users.init(
+  {
+    id: {
+      type: INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: STRING,
+      allowNull: false,
+    },
+    role: {
+      type: STRING,
+      allowNull: false,
+    },
+    email: {
+      type: STRING,
+      allowNull: false,
+    },
+    password: {
+      type: STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: db,
+    modelName: 'users',
+    timestamps: false,
+  },
+);
+
+export default Users;
